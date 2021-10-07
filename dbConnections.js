53be4fcc7f35d572c01fd78ec2e6db21d0e9844a
unknown <kh1amirhamza@gmail.com>
2021-10-07 21:02:10 +0600

    new

--- dbConnections.js
+++ dbConnections.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const config = {useNewUrlParser: true, useUnifiedTopology: true };
 const hellometDbConnectUrl = "mongodb+srv://hellomethealth:hellomethealth@cluster0.vrnxz.mongodb.net/hellomet?retryWrites=true&w=majority";
+//var URL = "mongodb+srv://ecommerce:amirhamza@cluster0.p4fnz.mongodb.net/?retryWrites=true&w=majority";
 
 module.exports = {
     getHellometDbConnection(){
